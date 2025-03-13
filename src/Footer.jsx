4a1ec1ec6cbbe847f772bb925c42e4c92f6869bf
Monika Januszek <monika.januszek.mail@gmail.com>
2025-03-13 13:16:05 +0100

    colors  & fonts declared scss

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import "./style/Footer.scss";
+
+const Footer = function () {
+  return (
+    <div className="footer">
+      <h className="title">To jest tytuł sekcji ą ę ó ł</h>
+      <p className="story">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p className="story">
+        Stokrotka, znana także jako Bellis perennis, jest uroczym, wieloletnim
+        kwiatem łąk i ogrodów.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
